803ecabf56a4e0017f570c29ecf6beab49185b61
Chupakabra0 <rootalfa4@gmail.com>
2022-06-15 18:02:25 +0300

    Project init

---
+++ PlotApp/MVVM/Views/CreatePlotView/CreatePlotView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PlotApp.MVVM.Views.CreatePlotView {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class CreatePlotView : Window {
+        public CreatePlotView() {
+            InitializeComponent();
+        }
+    }
+}
