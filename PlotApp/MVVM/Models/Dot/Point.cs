773aaef3157d1718f10fe46253ea027eb67069f6
Chupakabra0 <rootalfa4@gmail.com>
2022-06-15 19:20:48 +0300

    Add: point list and binding

---
+++ PlotApp/MVVM/Models/Dot/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotApp.MVVM.Models.Point {
+    internal class Point {
+        public Point() {
+            this.X = null;
+            this.Y = null;
+        }
+
+        public Point(double x, double y) {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double? X { get; set; }
+        public double? Y { get; set; }
+    }
+}
---
+++ PlotApp/MVVM/ViewModels/CreatePlotViewModel/CreatePlotViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+using Point = PlotApp.MVVM.Models.Point.Point;
+
+namespace PlotApp.MVVM.ViewModels.CreatePlotViewModel {
+    internal class CreatePlotViewModel : BaseViewModel.BaseViewModel {
+        public CreatePlotViewModel() {
+            this.Points = new ObservableCollection<Point>(
+                new List<Point> { new(1.0, 1.0), new(2.0, 2.0), new(3.0, 3.0) }
+            );
+        }
+
+        public ObservableCollection<Point> Points { get; set; }
+    }
+}
