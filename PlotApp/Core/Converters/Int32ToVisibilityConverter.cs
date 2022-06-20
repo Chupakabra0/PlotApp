f0688f17ab7a9496ac140ea3633420794b0093e0
Chupakabra0 <rootalfa4@gmail.com>
2022-06-20 20:42:40 +0300

    Add: collapsing of datagrid when functions list is empty

---
+++ PlotApp/Core/Converters/Int32ToVisibilityConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PlotApp.Core.Converters {
+    internal class Int32ToVisibilityConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            (int)value == 0 ? Visibility.Collapsed : Visibility.Visible;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            (Visibility)value == Visibility.Collapsed ? 0 : 1;
+    }
+}
