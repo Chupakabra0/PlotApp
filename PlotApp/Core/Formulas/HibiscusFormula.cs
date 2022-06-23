2f23216661847038ecb5e61a9100ca771657d4cf
Chupakabra0 <rootalfa4@gmail.com>
2022-06-23 19:38:36 +0300

    Added formulas

---
+++ PlotApp/Core/Formulas/HibiscusFormula.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlotApp.Core.Formulas {
+    internal class HibiscusFormula : IFormula {
+        public double Calculate(double x) {
+            return x switch {
+                > -4 and < -2 => 6.0 * x - 2.0 / Math.Tan(0.7 * x) + Math.Log(Math.Cos(2.0 * x)) / (2.0 * x + 4.0),
+                >  3 and <= 5 => Math.Sqrt(x),
+                _ => x + 4.0
+            };
+        }
+    }
+}
---
+++ PlotApp/Core/Formulas/MoneyFormula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlotApp.Core.Formulas {
+    internal class MoneyFormula : IFormula {
+        public double Calculate(double x) {
+            return x switch {
+                >  1 and <  2 => (Math.Sqrt(8.0 * x) * Math.Sin(5 * x * x) - 4.3)
+                                 / (Math.Pow(x, 1.0 / 3.0) + Math.Exp(2.0 * x) + Math.Abs(Math.Sin(2.0 * x))),
+                > -3 and <= 1 => Math.Pow(x, -2.0) - 2.0,
+                _             => Math.Pow(x, -2.0) - 4.0
+            };
+        }
+    }
+}
