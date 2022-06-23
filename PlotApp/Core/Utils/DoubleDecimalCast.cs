58aa01747991b4b431d70020bdf2ab5502768b3b
Chupakabra0 <rootalfa4@gmail.com>
2022-06-23 19:38:46 +0300

    Decimal cast fix

--- PlotApp/Core/Utils/DoubleDecimalCast.cs
+++ PlotApp/Core/Utils/DoubleDecimalCast.cs
@@ -1,33 +1,33 @@
 ﻿namespace PlotApp.Core.Utils {
     internal static class DoubleDecimalCast {
         public static double CastToDouble(decimal x) {
-            return (double)x;
+            try {
+                return x switch {
+                    // yeah, we need exactly equality, not "epsilon check"
+                    decimal.MaxValue => double.NaN,
+                    decimal.MinValue => double.NaN,
+                    _                => (double)x
+                };
+            }
+            catch {
+                return double.NaN;
+            }
         }
 
         public static decimal CastToDecimal(double x) {
             var DECIMAL_EPSILON = new decimal(1, 0, 0, false, 27);
 
             try {
-                // yeah, we need exactly equality, not "epsilon check"
-                if (x == double.Epsilon) {
-                    // decimal epsilon
-                    return DECIMAL_EPSILON;
-                }
-
-                if (x == double.PositiveInfinity) {
-                    // decimal infinity
-                    return 1M / DECIMAL_EPSILON;
-                }
-
-                if (x == double.NegativeInfinity) {
-                    // decimal -infinity
-                    return -1M / DECIMAL_EPSILON;
-                }
-
-                return(decimal)x;
+                return x switch {
+                    // yeah, we need exactly equality, not "epsilon check"
+                    double.Epsilon          => DECIMAL_EPSILON,
+                    double.PositiveInfinity => decimal.MaxValue,
+                    double.NegativeInfinity => decimal.MinValue,
+                    _                       => (decimal)x
+                };
             }
             catch {
-                return 1M / DECIMAL_EPSILON;
+                return decimal.MaxValue;
             }
         }
     }
