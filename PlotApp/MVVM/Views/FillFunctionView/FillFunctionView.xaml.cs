f05c8eafeba42dab90b32b6b638e2faba2027c87
Chupakabra0 <rootalfa4@gmail.com>
2022-06-24 17:35:15 +0300

    try-catch

--- PlotApp/MVVM/Views/FillFunctionView/FillFunctionView.xaml.cs
+++ PlotApp/MVVM/Views/FillFunctionView/FillFunctionView.xaml.cs
@@ -1,27 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace PlotApp.MVVM.Views.FillFunctionView
-{
+namespace PlotApp.MVVM.Views.FillFunctionView {
     /// <summary>
     /// Логика взаимодействия для FillFunctionView.xaml
     /// </summary>
-    public partial class FillFunctionView : Window
-    {
-        public FillFunctionView()
-        {
-            InitializeComponent();
+    public partial class FillFunctionView : Window {
+        public FillFunctionView() {
+            try {
+                InitializeComponent();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
